320c597afa1d914a5b4d91c2262f071c9ad8cee0
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2018-04-30 20:41:01 +0300

    Add simple to json mesher

--- kevlar_mesher/mesher.py
+++ kevlar_mesher/mesher.py
@@ -1,10 +1,7 @@
+import json
 import logging
+from typing import List, Callable
 
-from mpl_toolkits.mplot3d import Axes3D
-Axes3D
-
-import matplotlib.pyplot as plt
-import numpy as np
 from dataclasses import dataclass
 
 from . import config
@@ -20,42 +17,122 @@ LOGGER.setLevel(logging.INFO)
 
 
 @dataclass
+class Point:
+    x: float
+    y: float
+    z: float
+
+    def __add__(self, other: 'Point') -> 'Point':
+        assert isinstance(other, Point)
+        return Point(
+            self.x + other.x,
+            self.y + other.y,
+            self.z + other.z,
+        )
+
+    def to_json(self):
+        return dict(
+            x=self.x,
+            y=self.y,
+            z=self.z,
+        )
+
+
+@dataclass
+class Fiber:
+    points: List[Point]
+
+    def shift(self, dp: Point) -> 'Fiber':
+        new_points = list()
+        for p in self.points:
+            new_points.append(p + dp)
+
+        return Fiber(points=new_points)
+
+    def to_json(self):
+        return dict(
+            points=[p.to_json() for p in self.points]
+        )
+
+
+@dataclass
+class Layer:
+    fibers: List[Fiber]
+
+    def to_json(self):
+        return dict(
+            fibers=[f.to_json() for f in self.fibers]
+        )
+
+
+@dataclass
 class Mesh:
-    weft: np.ndarray
-    warp: np.ndarray
+    weft: Layer
+    warp: Layer
 
     def draw(self):
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        for f in self.weft:
-            ax.plot(f.transpose()[0], f.transpose()[1], f.transpose()[2], color='green')
-        for f in self.warp:
-            ax.plot(f.transpose()[0], f.transpose()[1], f.transpose()[2], color='red')
-        plt.show()
+        with open('mesh.json', mode='w+') as f:
+            json.dump(self.to_json(), f)
+
+    def to_json(self):
+        return dict(
+            weft=self.weft.to_json(),
+            warp=self.warp.to_json(),
+        )
+
+
+def create_warp(width: float, length: float, density: int) -> Layer:
+    def fn(t: float) -> Point:
+        return Point(x=t, y=0, z=0)
+
+    fibers_step = 1 / density
+    n_fibers = int(density * width)
+    points_step = 0.1
+    n_points = int(length / points_step)
+    root_fiber = create_fiber(fn, points_step, n_points)
+
+    fibers = [root_fiber]
+    dp = Point(x=0, y=fibers_step, z=0)
+    for i in range(n_fibers - 1):
+        fibers.append(fibers[-1].shift(dp=dp))
+
+    return Layer(fibers=fibers)
+
+
+def create_weft(width: float, length: float, density: int) -> Layer:
+    def fn(t: float) -> Point:
+        return Point(x=0, y=t, z=0)
+
+    fibers_step = 1 / density
+    n_fibers = int(density * width)
+    points_step = 0.1
+    n_points = int(length / points_step)
+    root_fiber = create_fiber(fn, points_step, n_points)
+
+    fibers = [root_fiber]
+    dp = Point(x=fibers_step, y=0, z=0)
+    for i in range(n_fibers - 1):
+        fibers.append(fibers[-1].shift(dp=dp))
+
+    return Layer(fibers=fibers)
+
+
+def create_fiber(fn: Callable[[float], Point], step: float, n_points: int) -> Fiber:
+    points = []
+
+    t = 0
+    for i in range(n_points):
+        points.append(fn(t))
+        t += step
+
+    return Fiber(points=points)
 
 
 def create_mesh(cfg: config.Config) -> Mesh:
     LOGGER.info('start meshing...')
 
-    n_warp_fibers = int(cfg.width * cfg.warp_density)
-    n_points_for_warp_fiber = int(cfg.length * cfg.resolution)
-
-    warp = np.zeros(shape=(n_warp_fibers, n_points_for_warp_fiber, DIM))
-    warp_fibers_diff = np.array([0, 1 / cfg.warp_density, 0])
-    warp_points_diff = np.array([1 / cfg.resolution, 0, 0])
-    for i in range(n_warp_fibers):
-        for j in range(n_points_for_warp_fiber):
-            warp[i][j] = i * warp_fibers_diff + j * warp_points_diff
-
-    n_weft_fibers = int(cfg.length * cfg.weft_density)
-    n_points_for_weft_fiber = int(cfg.width * cfg.resolution)
-
-    weft = np.zeros(shape=(n_weft_fibers, n_points_for_weft_fiber, DIM))
-    weft_fibers_diff = np.array([1 / cfg.weft_density, 0, 0])
-    weft_points_diff = np.array([0, 1 / cfg.resolution, 0])
-    for i in range(n_weft_fibers):
-        for j in range(n_points_for_weft_fiber):
-            weft[i][j] = i * weft_fibers_diff + j * weft_points_diff
+    warp = create_warp(cfg.width, cfg.length, cfg.warp_density)
+    weft = create_weft(cfg.length, cfg.width, cfg.weft_density)
 
     LOGGER.info('end meshing...')
 
