2fd32d01c9433de6d688f70eeb08caf5123e01ef
Ivan Kosolapov <ikosolapov@express42.com>
2018-12-13 20:04:08 +0300

    Add simple collision detector

--- kevlar_mesher/__main__.py
+++ kevlar_mesher/__main__.py
@@ -1,4 +1,5 @@
 import time
+from itertools import islice
 
 import yaml
 
@@ -17,18 +18,16 @@ def main():
     for n, task in enumerate(cfg.tasks):
         try:
             start_time = time.monotonic()
-            _LOGGER.info(
-                f'Processing task #{n+1} of {len(cfg.tasks)}: {task.name}')
+            _LOGGER.info(f'Processing task #{n+1} of {len(cfg.tasks)}: {task.name}')
             mesh = mesher.create_mesh(task.mesh)
             _LOGGER.info('Mesh created')
             writer.save(mesh, cfg.out_dir, f'{task.name}_mesh')
 
             _LOGGER.info('Solving')
-            results_generator = solver.solve(mesh, task.solver)
+            results_generator = islice(solver.solve(mesh, task.solver), task.solver.n_steps)
             for (idx, res) in enumerate(results_generator):
                 writer.save(res, cfg.out_dir, f'{task.name}-{idx:04d}')
-                _LOGGER.info(
-                    f'Done step {idx+ 1:d} of {task.solver.n_steps:d}')
+                _LOGGER.info(f'Done step {idx+ 1:d} of {task.solver.n_steps:d}')
             _LOGGER.info(f'Solved, took {time.monotonic() - start_time:.2f}s')
         except Exception as e:
             _LOGGER.error(f'Error while processing task #{n+1}: {e}')
